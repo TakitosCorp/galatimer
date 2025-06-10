adae177f2a9a79a49e11502f8c6bcab219f4d68a
AlexDeveloperUwU <79850072+AlexDeveloperUwU@users.noreply.github.com>
2025-06-10 20:37:41 +0200

    fix: :bug: Expose port

--- init.sh
+++ init.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Borrar la imagen vieja de Docker
 docker compose down --rmi all
 
-# Compilar la imagen de Docker
 docker compose build
 
-# Iniciar los contenedores en segundo plano
 docker compose up -d
